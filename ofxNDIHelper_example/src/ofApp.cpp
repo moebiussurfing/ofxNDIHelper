2e31d8cbd12af0677b51451068630e42a95f6e9c
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-09-09 01:43:17 -0300

    update

--- ofxNDIHelper_example/src/ofApp.cpp
+++ ofxNDIHelper_example/src/ofApp.cpp
@@ -82,53 +82,8 @@ void ofApp::keyPressed(int key) {
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
 void ofApp::windowResized(int w, int h) {
 #ifdef USE_ofxNDI
 	NDIHelper.windowResized(w, h);
 #endif
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) {
-
 }
--- ofxNDIHelper_example/src/ofApp.h
+++ ofxNDIHelper_example/src/ofApp.h
@@ -20,18 +20,8 @@ public:
 	void update();
 	void draw();
 	void exit();
-
 	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void mouseEntered(int x, int y);
-	void mouseExited(int x, int y);
 	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
 
 public:
 #ifdef USE_ofxNDI
